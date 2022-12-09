17eb8aef2ce9795c1aae31c5f5474d57eba72573
unknown <luisdmartinezu06@gmail.com>
2022-12-09 14:57:27 -0500

    [add]: getbyday

--- src/controllers/medidor.controller.js
+++ src/controllers/medidor.controller.js
@@ -13,7 +13,37 @@ const getAsync = async (req, res) => {
         res.send(error.message);
     }
 }
+const getByDayAsync = async (req, res) => {
+    try {
+        const {date } = req.body;
+        console.log(typeof(date));
+        let medidorList = [
+            {date: "2022-12-25 15:26:12"},
+            {date: "2022-12-25 16:26:12"},
+            {date: "2022-12-25 17:26:12"},
+            {date: "2022-12-25 18:26:12"},
+            {date: "2022-12-25 19:26:12"},
+        ];
+        medidorList = medidorList.filter(meditorItem => {
+            let dateString = meditorItem.date.split(" ")[0];
+            return dateString === date;
+        });
+        console.log(medidorList);
+
+
+        // let medidorList = await Medidor.findAll({
+        //     order: [["date", "asc"]]
+        // });
+        // let obj = JSON.parse(JSON.stringify(medidorList[0]));
+        // console.log(obj);
+        res.json(medidorList);
+    } catch (error) {
+        res.status(500)
+        res.send(error.message);
+    }
+}
 
 export const methods = {
-    getAsync
+    getAsync,
+    getByDayAsync
 }
--- src/routes/medidor.routes.js
+++ src/routes/medidor.routes.js
@@ -4,5 +4,6 @@ import { methods as medidorController } from "../controllers/medidor.controller"
 const router = Router();
 
 router.get("/", medidorController.getAsync);
+router.post("/day", medidorController.getByDayAsync);
 
 export default router;
